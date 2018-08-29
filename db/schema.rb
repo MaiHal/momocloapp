64740e09453b1f788abba0748fbd7be4eb0c30be
MaiHal <myturn.lovenyc@gmail.com>
2018-08-29 14:58:54 +0900

    テーブルの内容変更・usersテーブルへの保存処理を作成

--- db/schema.rb
+++ db/schema.rb
@@ -26,9 +26,9 @@ ActiveRecord::Schema.define(version: 2018_08_12_142327) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "users"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string "name", null: false
+    t.string "email", null: false
+    t.string "password", null: false
   end
 
 end
