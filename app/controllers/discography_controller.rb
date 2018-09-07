cd840004b6ac9fcf7bd4eee7e9f254d9600c59b9
MaiHal <myturn.lovenyc@gmail.com>
2018-09-07 22:26:23 +0900

    Discographyページに画像を表示

--- app/controllers/discography_controller.rb
+++ app/controllers/discography_controller.rb
@@ -1,5 +1,6 @@
 class DiscographyController < ApplicationController
 	def index 
-		@discography = Discography.limit(3).offset(4)
+		@discography = Discography.all
+		@albums = Album.order(id: :desc)
 	end
 end
